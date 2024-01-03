0186771b6b6f5223aec8f9fc299dc5414b6669f8
Edwin Tang <edwintang10@gmail.com>
2024-01-03 15:24:10 +0800

    feat(scanner): add support for reserved words and identifiers

--- src/main/java/software/full_stack/Scanner.java
+++ src/main/java/software/full_stack/Scanner.java
@@ -1,16 +1,40 @@
 package software.full_stack;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static software.full_stack.TokenType.*;
 
 public class Scanner {
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+
+    }
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
     private int start = 0;
-    private int current = 1;
+    private int current = 0;
 
     private int line = 1;
 
@@ -51,7 +75,7 @@ public class Scanner {
                 addToken(match('=') ? LESS_EQUAL : LESS);
                 break;
             case '>':
-                addToken(match('=') ? GREATER_EQUAL : GREATER_EQUAL);
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
             case '/':
                 if(match('/')) {
@@ -71,9 +95,38 @@ public class Scanner {
                 break;
             case '"': string(); break;
             default:
-                Lox.error(line, "Unexpected character.");
+                if(isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        while(isDigit(peek())) advance();
+        // Look for a fractional part
+        if (peek() ==  '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while(isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER,
+                Double.parseDouble(source.substring(start, current)));
     }
 
     // Read string until the closing apostrophe
@@ -90,6 +143,7 @@ public class Scanner {
 
         // The closing "
         advance();
+
         String value = source.substring(start + 1, current - 1);
         addToken(STRING, value);
     }
@@ -105,6 +159,25 @@ public class Scanner {
     private char peek() {
         if(isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() {
